cb7ab989ae596e7cf39d4c358efabd8263ba36a3
ritych <ritych@gmail.com>
2018-01-10 09:44:17 +0300

    add cocntoller
    transferred the code from the route to the controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,22 +7,66 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
-    }
+	public function front(){
+		$links = \App\Ingridient::all();
+		return view('front', compact('links'));
+	}
+	
+	public function showorder($id) {
+		$orders = \App\Order::where('id', $id)->get();
+		return view('order', compact('orders'));
+	}
+	
+	public function submit (Request $request) {
+		$data = $request->validate([
+			'name' => 'required|max:255',
+			'phone' => 'required|max:255',
+		]);
+
+		// $order = tap(new App\Order($data))->save();
+		$order = new \App\Order;
+		$order->name = $data['name'];
+		$order->phone = $data['phone'];
+	
+		$ingridient = \App\Ingridient::all();
+		$total_sum=0;
+		$i=0;
+		foreach ($request->post() as $key => $value){
+			if (isset($value)){
+				if ($key!='size'){
+				
+					if ($key!='name' && $key!='phone' && $key!='op'){
+						$data=explode('_', $key);
+						$wrkingr=$ingridient->where('id', $data[1]);
+					
+						foreach ($wrkingr as $ingr){
+							$desc[$data[0]][$i]['title']=$ingr->title;
+							$desc[$data[0]][$i]['cost']=$ingr->cost;
+							$desc[$data[0]][$i]['count']=$value;
+							$total_sum+=$ingr->cost*$value;
+							$i++;
+						}
+					}
+				}
+				else{
+					$wrkingr=$ingridient->where('id', $value);
+					foreach ($wrkingr as $size){
+						$desc['size']['title']=$size->title;
+						$desc['size']['cost']=$size->cost;
+						$total_sum+=$size->cost;
+					}
+				}
+			}
+		}
+		$desc['total']=$total_sum;
+		$desc['name']=$request->post('name');
+		$desc['phone']=$request->post('phone');
+		$order->description = serialize($desc);
+		$order->save();
+		return redirect('/order/'.$order->id);
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -12,67 +12,11 @@
 */
 
 
-Route::get('/', function () {
-	$links = \App\Ingridient::all();
-	return view('front', compact('links'));
-});
+Route::get('/', 'HomeController@front');
 
-Route::get('/order/{id}', function ($id) {
-	$orders = \App\Order::where('id', $id)->get();
-	return view('order', compact('orders'));
-});
+Route::get('/order/{id}', 'HomeController@showorder');
 
-use Illuminate\Http\Request;
-
-Route::post('/submit', function (Request $request) {
-	$data = $request->validate([
-        'name' => 'required|max:255',
-        'phone' => 'required|max:255',
-    ]);
-
-    // $order = tap(new App\Order($data))->save();
-	$order = new App\Order;
-	$order->name = $data['name'];
-	$order->phone = $data['phone'];
-	
-	$ingridient = \App\Ingridient::all();
-	$total_sum=0;
-	$i=0;
-	foreach ($request->post() as $key => $value){
-		if (isset($value)){
-			if ($key!='size'){
-				
-				if ($key!='name' && $key!='phone' && $key!='op'){
-					$data=explode('_', $key);
-					$wrkingr=$ingridient->where('id', $data[1]);
-					
-					foreach ($wrkingr as $ingr){
-						$desc[$data[0]][$i]['title']=$ingr->title;
-						$desc[$data[0]][$i]['cost']=$ingr->cost;
-						$desc[$data[0]][$i]['count']=$value;
-						$total_sum+=$ingr->cost*$value;
-						$i++;
-					}
-				}
-			}
-			else{
-				$wrkingr=$ingridient->where('id', $value);
-				foreach ($wrkingr as $size){
-					$desc['size']['title']=$size->title;
-					$desc['size']['cost']=$size->cost;
-					$total_sum+=$size->cost;
-				}
-			}
-		}
-	}
-	$desc['total']=$total_sum;
-	$desc['name']=$request->post('name');
-	$desc['phone']=$request->post('phone');
-	$order->description = serialize($desc);
-	$order->save();
-    return redirect('/order/'.$order->id);
-});
+Route::post('/submit', 'HomeController@submit');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
